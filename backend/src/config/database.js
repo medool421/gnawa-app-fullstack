a295904fe4153eb5ae512075fa3b4802b49ff7b2
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2025-12-12 01:00:09 +0100

    database connection setup

---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false
+});
+
+module.exports = sequelize;
